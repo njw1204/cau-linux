3586ce66264001785952b367b922f9e9f722d2fc
Na Jongwoo <njw1204@naver.com>
2022-09-21 23:26:55 +0900

    Validate multi table input

--- multi-table.sh
+++ multi-table.sh
@@ -1,2 +1,11 @@
 #!/bin/bash
 
+if [ $# -ne 2 ]; then
+  echo "Invalid number of input"
+  exit 1
+fi
+
+if [[ ! ($1 =~ ^[1-9][0-9]*$) ]] || [[ ! ($2 =~ ^[1-9][0-9]*$) ]]; then
+  echo "Input must be positive integer"
+  exit 1
+fi
