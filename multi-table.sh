d061d6d8fe3851f83dfce057bfd727311f046301
Na Jongwoo <njw1204@naver.com>
2022-09-21 23:28:02 +0900

    Print all multi table

--- multi-table.sh
+++ multi-table.sh
@@ -9,3 +9,23 @@ if [[ ! ($1 =~ ^[1-9][0-9]*$) ]] || [[ ! ($2 =~ ^[1-9][0-9]*$) ]]; then
   echo "Input must be positive integer"
   exit 1
 fi
+
+i=1
+
+while [ $i -le $1 ]
+do
+  j=1
+
+  while [ $j -le $2 ]
+  do
+    if [ $j -ne 1 ]; then
+      printf "\t"
+    fi
+
+    printf "$i*$j=$((i*j))"
+    j=$((j+1))
+  done
+
+  i=$((i+1))
+  printf "\n"
+done
