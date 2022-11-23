1cccae7d84b01695d1f4787fee9edf3feb7e2048
Na Jongwoo <njw1204@naver.com>
2022-11-23 15:56:53 +0900

    Add sqrt_list.h alpha version

--- sqrt-list/linux/sqrt_list.h
+++ sqrt-list/linux/sqrt_list.h
@@ -8,11 +8,20 @@
 #include <linux/const.h>
 #include <linux/kernel.h>
 
+#define SQRT_LIST_POISON1 ((void *) 0x100 + POISON_POINTER_DELTA)
+#define SQRT_LIST_POISON2 ((void *) 0x200 + POISON_POINTER_DELTA)
+#define SQRT_LIST_POISON3 ((void *) 0x300 + POISON_POINTER_DELTA)
+#define SQRT_LIST_POISON4 ((void *) 0x400 + POISON_POINTER_DELTA)
+#define SQRT_LIST_POISON5 ((void *) 0x500 + POISON_POINTER_DELTA)
+
 struct sqrt_list_head {
-	struct sqrt_list_head *next, *prev;
+	struct sqrt_list_head *next, *prev, *head, *bucket;
+	struct sqrt_list_head *b_next, *b_prev; // only for bucket(and also head)
+	unsigned long b_next_gap; // only for bucket(and also head)
+	unsigned long h_base_gap, h_size; // only for head
 };
 
-#define SQRT_LIST_HEAD_INIT(name) { &(name), &(name) }
+#define SQRT_LIST_HEAD_INIT(name) { &(name), &(name), &(name), &(name), NULL, NULL, 0, 2, 0 }
 
 #define SQRT_LIST_HEAD(name) \
 	struct sqrt_list_head name = SQRT_LIST_HEAD_INIT(name)
@@ -21,6 +30,13 @@ static inline void INIT_SQRT_LIST_HEAD(struct sqrt_list_head *list)
 {
 	WRITE_ONCE(list->next, list);
 	list->prev = list;
+	list->head = list;
+	list->bucket = list;
+	list->b_next = NULL;
+	list->b_prev = NULL;
+	list->b_next_gap = 0;
+	list->h_base_gap = 2;
+	list->h_size = 0;
 }
 
 #ifdef CONFIG_DEBUG_SQRT_LIST
@@ -41,6 +57,88 @@ static inline bool __sqrt_list_del_entry_valid(struct sqrt_list_head *entry)
 }
 #endif
 
+static inline unsigned long ___sqrt_list_calc_proper_gap(struct sqrt_list_head *head)
+{
+	unsigned long gap = int_sqrt(head->h_size);
+	return (gap > 2) ? gap : 2;
+}
+
+// amortized O(sqrt(n))
+// O(n) cost, once per O(sqrt(n)) times = O(n/sqrt(n)) = O(sqrt(n))
+static inline void ___sqrt_list_arrange_buckets(struct sqrt_list_head *head)
+{
+	struct sqrt_list_head *pos, *tmp;
+	unsigned long passed;
+	unsigned long gap = ___sqrt_list_calc_proper_gap(head);
+
+	if (((gap > head->h_base_gap) ? gap - head->h_base_gap : head->h_base_gap - gap) < 2) {
+		return;
+	}
+
+	head->bucket = head;
+	head->b_next = NULL;
+	head->b_prev = NULL;
+	head->b_next_gap = 0;
+	head->h_base_gap = gap;
+
+	for (pos = head->next, tmp = head, passed = 0; pos != head; pos = pos->next) {
+		pos->bucket = tmp;
+
+		if (++passed == gap) {
+			passed = 0;
+			pos->bucket = pos;
+			tmp->b_next = pos;
+			pos->b_prev = tmp;
+			tmp->b_next_gap = gap;
+			tmp = pos;
+		}
+	}
+}
+
+static inline void __sqrt_list_assign_to_bucket(struct sqrt_list_head *entry, struct sqrt_list_head *bucket)
+{
+	entry->head = bucket->head;
+	entry->bucket = bucket;
+	bucket->b_next_gap++;
+	bucket->head->h_size++;
+	___sqrt_list_arrange_buckets(bucket->head);
+}
+
+static inline void __sqrt_list_remove_from_bucket(struct sqrt_list_head *entry, struct sqrt_list_head *bucket)
+{
+	bucket->b_next_gap--;
+	bucket->head->h_size--;
+
+	if (bucket == entry) {
+		if (bucket->next == bucket->head) {
+			bucket->b_prev->b_next = NULL;
+		} else if (bucket->next == bucket->b_next) {
+			bucket->b_prev->b_next = bucket->b_next;
+			bucket->b_next->b_prev = bucket->b_prev;
+		} else {
+			struct sqrt_list_head *pos;
+			struct sqrt_list_head *new_bucket = bucket->next;
+			new_bucket->b_next_gap = 0;
+
+			// O(sqrt(n)) - change bucket
+			for (pos = new_bucket; pos && pos->bucket == bucket; pos = pos->next) {
+				pos->bucket = new_bucket;
+				new_bucket->b_next_gap++;
+			}
+
+			new_bucket->b_prev = bucket->b_prev;
+			new_bucket->b_next = bucket->b_next;
+			new_bucket->b_prev->b_next = new_bucket;
+
+			if (new_bucket->b_next) {
+				new_bucket->b_next->b_prev = new_bucket;
+			}
+		}
+	}
+
+	___sqrt_list_arrange_buckets(bucket->head);
+}
+
 static inline void __sqrt_list_add(struct sqrt_list_head *new,
 			      struct sqrt_list_head *prev,
 			      struct sqrt_list_head *next)
@@ -52,6 +150,7 @@ static inline void __sqrt_list_add(struct sqrt_list_head *new,
 	new->next = next;
 	new->prev = prev;
 	WRITE_ONCE(prev->next, new);
+	__sqrt_list_assign_to_bucket(new, prev->bucket);
 }
 
 static inline void sqrt_list_add(struct sqrt_list_head *new, struct sqrt_list_head *head)
@@ -64,25 +163,24 @@ static inline void sqrt_list_add_tail(struct sqrt_list_head *new, struct sqrt_li
 	__sqrt_list_add(new, head->prev, head);
 }
 
-static inline void __sqrt_list_del(struct sqrt_list_head *prev, struct sqrt_list_head *next)
-{
-	next->prev = prev;
-	WRITE_ONCE(prev->next, next);
-}
-
 static inline void __sqrt_list_del_entry(struct sqrt_list_head *entry)
 {
 	if (!__sqrt_list_del_entry_valid(entry))
 		return;
 
-	__sqrt_list_del(entry->prev, entry->next);
+	entry->next->prev = entry->prev;
+	WRITE_ONCE(entry->prev->next, entry->next);
+	__sqrt_list_remove_from_bucket(entry, entry->bucket);
 }
 
 static inline void sqrt_list_del(struct sqrt_list_head *entry)
 {
 	__sqrt_list_del_entry(entry);
-	entry->next = LIST_POISON1;
-	entry->prev = LIST_POISON2;
+	entry->next = SQRT_LIST_POISON1;
+	entry->prev = SQRT_LIST_POISON2;
+	entry->head = SQRT_LIST_POISON3;
+	entry->bucket = SQRT_LIST_POISON4;
+	entry->b_next = SQRT_LIST_POISON5;
 }
 
 static inline void sqrt_list_del_init(struct sqrt_list_head *entry)
@@ -130,6 +228,21 @@ static inline int sqrt_list_empty_careful(const struct sqrt_list_head *head)
 static inline int sqrt_list_is_singular(const struct sqrt_list_head *head)
 {
 	return !sqrt_list_empty(head) && (head->next == head->prev);
+}
+
+static inline struct sqrt_list_head *sqrt_list_nth(struct sqrt_list_head *head, unsigned long n)
+{
+	struct sqrt_list_head *pos;
+
+	if (n < 1 || n > head->h_size) {
+		return NULL;
+	}
+
+	// O(sqrt(n)) - find bucket
+	for (pos = head; n >= pos->b_next_gap && pos->b_next; n -= pos->b_next_gap, pos = pos->b_next);
+	// O(sqrt(n)) - find offset
+	for (; n--; pos = pos->next);
+	return pos;
 }
 
 #define sqrt_list_entry(ptr, type, member) \
